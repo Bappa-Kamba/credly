6a60754ea65d1bf72e6b3c83c95e1ec3074ffc13
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-09-23 12:46:14 +0100

    Refactor form layout and styling

---
+++ backend/config.py
@@ -0,0 +1,3 @@
+WAEC_DIRECT = 'https://www.waecdirect.org/DisplayResult.aspx'
+WAEC_ONLINE = 'https://verify.waeconline.org.ng/Home/InstantVerification'
+NECO = 'https://www.necoproductions.com:8095/api/results/check?exam_year=2024&exam_type=ssce_int&reg_no=2410018877GI&token=528492244243'
