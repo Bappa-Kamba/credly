0308fd46081b4eefb40e3c135c982a246e0aab5a
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-09-23 12:47:48 +0100

    Remove unnecessary backend files and dependencies

--- backend/src/config.js
+++
@@ -1,3 +0,0 @@
-export const API_URL = 'https://verify.waeconline.org.ng/Home/InstantResultVerification';
-export const PORT = process.env.PORT || 5000;
-
--- backend/src/routes/verificationRoutes.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import  { requestHandler } from '../controllers/verificationController.js';
-const router = express.Router();
-
-export default router.post('/', requestHandler);
